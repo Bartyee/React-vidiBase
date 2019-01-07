3e5e9060932fa179eeedb5e83d8909add66d9d59
Bartyee <bpawelec998@gmail.com>
2019-01-07 18:12:28 +0100

    Updates

---
+++ precache-manifest.0ac3e1df37e5977c302bef121e92d968.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "44abc87de3b0ebb6ddcdfef7e9d560fb",
+    "url": "/React-vidiBase/movie/297802/static/media/play-button.44abc87d.svg"
+  },
+  {
+    "revision": "7c8905c9a0d12f6a53ee6ff6fdfa55ef",
+    "url": "/React-vidiBase/movie/297802/static/media/magnifier.7c8905c9.svg"
+  },
+  {
+    "revision": "ce7a274a7cd3e5218660f0bec1e2a4b4",
+    "url": "/React-vidiBase/movie/297802/static/media/list-menu.ce7a274a.svg"
+  },
+  {
+    "revision": "c63b54137d564433705fb3594dbb617e",
+    "url": "/React-vidiBase/movie/297802/static/media/No_image.c63b5413.jpg"
+  },
+  {
+    "revision": "9e6c5ace317b80a81f71",
+    "url": "/React-vidiBase/movie/297802/static/js/runtime~main.9e6c5ace.js"
+  },
+  {
+    "revision": "6b6949d0cd947a266512",
+    "url": "/React-vidiBase/movie/297802/static/js/main.6b6949d0.chunk.js"
+  },
+  {
+    "revision": "4f3b9127c9ee4744dc2f",
+    "url": "/React-vidiBase/movie/297802/static/js/1.4f3b9127.chunk.js"
+  },
+  {
+    "revision": "6b6949d0cd947a266512",
+    "url": "/React-vidiBase/movie/297802/static/css/main.b6e82d04.chunk.css"
+  },
+  {
+    "revision": "5b48faa8fa421f0507a599742bee8647",
+    "url": "/React-vidiBase/movie/297802/index.html"
+  }
+];
