efe01bfd1a7a133f3930efb451e60f6401ee8035
Bartyee <bpawelec998@gmail.com>
2019-01-07 18:43:49 +0100

    Updates

---
+++ precache-manifest.3e10c232fa38370780328d718be409e1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "44abc87de3b0ebb6ddcdfef7e9d560fb",
+    "url": "/React-vidiBase/static/media/play-button.44abc87d.svg"
+  },
+  {
+    "revision": "7c8905c9a0d12f6a53ee6ff6fdfa55ef",
+    "url": "/React-vidiBase/static/media/magnifier.7c8905c9.svg"
+  },
+  {
+    "revision": "ce7a274a7cd3e5218660f0bec1e2a4b4",
+    "url": "/React-vidiBase/static/media/list-menu.ce7a274a.svg"
+  },
+  {
+    "revision": "c63b54137d564433705fb3594dbb617e",
+    "url": "/React-vidiBase/static/media/No_image.c63b5413.jpg"
+  },
+  {
+    "revision": "9c741baefb02d4f7850e",
+    "url": "/React-vidiBase/static/js/runtime~main.9c741bae.js"
+  },
+  {
+    "revision": "9ef84cbab037b4168c02",
+    "url": "/React-vidiBase/static/js/main.9ef84cba.chunk.js"
+  },
+  {
+    "revision": "4f3b9127c9ee4744dc2f",
+    "url": "/React-vidiBase/static/js/1.4f3b9127.chunk.js"
+  },
+  {
+    "revision": "9ef84cbab037b4168c02",
+    "url": "/React-vidiBase/static/css/main.b6e82d04.chunk.css"
+  },
+  {
+    "revision": "bf9137271eda15f6313e80012ee88ac3",
+    "url": "/React-vidiBase/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4a48eec8761b7c343c1f3a0f8d3c366a.js"
+  "/React-vidiBase/precache-manifest.3e10c232fa38370780328d718be409e1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-vidiBase/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
